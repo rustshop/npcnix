4fd6affa733d18a0b28813a949fb833da8311a99
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-04-29 19:31:15 -0700

    chore: expose system module

--- flake.nix
+++ flake.nix
@@ -8,124 +8,146 @@
     crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, crane }: {
+    nixosModules = {
+      npcnix = import ./modules/npcnix.nix { inherit self; };
+      default = self.nixosModules.npcnix;
+    };
+
+    nixosConfigurations =
       let
+        system = "x86_64-linux";
         pkgs = import nixpkgs {
           inherit system;
         };
-        lib = pkgs.lib;
-        craneLib = crane.lib.${system};
-
-        commonArgs =
-
-          let
-            # Only keeps markdown files
-            readmeFilter = path: _type: builtins.match ".*/README\.md$" path != null;
-            markdownOrCargo = path: type:
-              (readmeFilter path type) || (craneLib.filterCargoSources path type);
-          in
-          {
-            doCheck = false;
-
-            src = lib.cleanSourceWith {
-              src = craneLib.path ./.;
-              filter = markdownOrCargo;
-            };
-
-            buildInputs = with pkgs; [
-              openssl
-              pkg-config
-            ] ++ lib.optionals stdenv.isDarwin [
-              libiconv
-              darwin.apple_sdk.frameworks.Security
-            ];
-
-            nativeBuildInputs = [
-            ];
-          };
-        npcnixPkgUnwrapped = craneLib.buildPackage ({ } // commonArgs);
-        npcnixPkgWrapped = pkgs.writeShellScriptBin "npcnix" ''
-          exec env \
-            NPCNIX_AWS_CLI=''${NPCNIX_AWS_CLI:-${pkgs.awscli2}/bin/aws} \
-            NPCNIX_NIXOS_REBUILD=''${NPCNIX_NIXOS_REBUILD:-${pkgs.nixos-rebuild}/bin/nixos-rebuild} \
-            ${npcnixPkgUnwrapped}/bin/npcnix "$@"
-        '';
       in
       {
-        packages =
-          {
-            default = npcnixPkgWrapped;
-            npcnix-unwrapped = npcnixPkgUnwrapped;
-            npcnix = npcnixPkgWrapped;
-            install = pkgs.writeShellScriptBin "npcnix-install" ''
-              set -e
-              npcnix_swapfile="/npcnix-install-swapfile"
+        basic = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
 
-              function cleanup() {
-                if [ -e "$npcnix_swapfile" ]; then
-                  >&2 echo "Cleaning up temporary swap file..."
-                  ${pkgs.util-linux}/bin/swapoff "$npcnix_swapfile" || true
-                  ${pkgs.coreutils}/bin/rm -f "$npcnix_swapfile" || true
-                fi
-              }
-              # clean unconditionally, in case we left over something in a previous run, etc.
-              trap cleanup EXIT
+          modules = [
 
-              if [ "$(${pkgs.util-linux}/bin/swapon --noheadings --raw | ${pkgs.coreutils}/bin/wc -l )" = "0" ] ; then
-                >&2 echo "No swap detected. Creating a temporary swap file..."
-                if [ ! -e "$npcnix_swapfile" ]; then
-                  # it has been experimentally verified, that 2G should be enough
-                  # bootstrap even on AWS EC2 t3.nano instances
-                  ${pkgs.util-linux}/bin/fallocate -l 2G "$npcnix_swapfile" || true
-                fi
-                chmod 600 "$npcnix_swapfile" && \
-                  ${pkgs.util-linux}/bin/mkswap "$npcnix_swapfile" && \
-                  ${pkgs.util-linux}/bin/swapon "$npcnix_swapfile" || \
-                  true
-              fi
-
-              ${npcnixPkgWrapped}/bin/npcnix install "$@"
-              cleanup
-            '';
-          };
+            ({ modulesPath, ... }: {
+              imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+              ec2.hvm = true;
 
 
-        devShells = {
-          default = pkgs.mkShell {
+              system.stateVersion = "22.11";
+            })
 
-            buildInputs = [ ] ++ commonArgs.buildInputs;
-            nativeBuildInputs = with pkgs; [
-              rust-analyzer
-              cargo-udeps
-              typos
-              cargo
-              rustc
-              rustfmt
-              clippy
-
-              # This is required to prevent a mangled bash shell in nix develop
-              # see: https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
-              (hiPrio pkgs.bashInteractive)
-
-              # Nix
-              pkgs.nixpkgs-fmt
-              pkgs.shellcheck
-              pkgs.rnix-lsp
-              pkgs.nodePackages.bash-language-server
-
-            ] ++ commonArgs.nativeBuildInputs;
-            shellHook = ''
-              dot_git="$(git rev-parse --git-common-dir)"
-              if [[ ! -d "$dot_git/hooks" ]]; then
-                  mkdir "$dot_git/hooks"
-              fi
-              for hook in misc/git-hooks/* ; do ln -sf "$(pwd)/$hook" "$dot_git/hooks/" ; done
-              ${pkgs.git}/bin/git config commit.template $(pwd)/misc/git-hooks/commit-template.txt
-            '';
-          };
+            self.nixosModules.default
+          ];
         };
-      }
-    );
+      };
+  }
+  // flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      lib = pkgs.lib;
+      craneLib = crane.lib.${system};
+
+      commonArgs =
+
+        let
+          # Only keeps markdown files
+          readmeFilter = path: _type: builtins.match ".*/README\.md$" path != null;
+          markdownOrCargo = path: type:
+            (readmeFilter path type) || (craneLib.filterCargoSources path type);
+        in
+        {
+          doCheck = false;
+
+          src = lib.cleanSourceWith {
+            src = craneLib.path ./.;
+            filter = markdownOrCargo;
+          };
+
+          buildInputs = [
+            pkgs.openssl
+            pkgs.pkg-config
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
+          ];
+
+          nativeBuildInputs = [ ];
+        };
+      npcnixPkgUnwrapped = craneLib.buildPackage ({ } // commonArgs);
+      npcnixPkgWrapped = pkgs.writeShellScriptBin "npcnix" ''
+        exec env \
+          NPCNIX_AWS_CLI=''${NPCNIX_AWS_CLI:-${pkgs.awscli2}/bin/aws} \
+          NPCNIX_NIXOS_REBUILD=''${NPCNIX_NIXOS_REBUILD:-${pkgs.nixos-rebuild}/bin/nixos-rebuild} \
+          ${npcnixPkgUnwrapped}/bin/npcnix "$@"
+      '';
+    in
+    {
+      packages = {
+        default = npcnixPkgWrapped;
+        npcnix-unwrapped = npcnixPkgUnwrapped;
+        npcnix = npcnixPkgWrapped;
+        install = pkgs.writeShellScriptBin "npcnix-install" ''
+          set -e
+          npcnix_swapfile="/npcnix-install-swapfile"
+
+          function cleanup() {
+            if [ -e "$npcnix_swapfile" ]; then
+              >&2 echo "Cleaning up temporary swap file..."
+              ${pkgs.util-linux}/bin/swapoff "$npcnix_swapfile" || true
+              ${pkgs.coreutils}/bin/rm -f "$npcnix_swapfile" || true
+            fi
+          }
+          # clean unconditionally, in case we left over something in a previous run, etc.
+          trap cleanup EXIT
+
+          if [ "$(${pkgs.util-linux}/bin/swapon --noheadings --raw | ${pkgs.coreutils}/bin/wc -l )" = "0" ] ; then
+            >&2 echo "No swap detected. Creating a temporary swap file..."
+            if [ ! -e "$npcnix_swapfile" ]; then
+              # it has been experimentally verified, that 2G should be enough
+              # bootstrap even on AWS EC2 t3.nano instances
+              ${pkgs.util-linux}/bin/fallocate -l 2G "$npcnix_swapfile" || true
+            fi
+            chmod 600 "$npcnix_swapfile" && \
+              ${pkgs.util-linux}/bin/mkswap "$npcnix_swapfile" && \
+              ${pkgs.util-linux}/bin/swapon "$npcnix_swapfile" || \
+              true
+          fi
+
+          ${npcnixPkgWrapped}/bin/npcnix install "$@"
+          cleanup
+        '';
+      };
+
+      checks = {
+        nixosConfiguration = self.nixosConfigurations.basic.config.system.build.toplevel;
+      };
+
+
+      devShells = {
+        default = pkgs.mkShell {
+
+          buildInputs = [ ] ++ commonArgs.buildInputs;
+          nativeBuildInputs = builtins.attrValues
+            {
+              inherit (pkgs) rust-analyzer cargo-udeps typos cargo rustc rustfmt clippy just nixpkgs-fmt shellcheck rnix-lsp;
+            } ++ [
+            pkgs.nodePackages.bash-language-server
+            # This is required to prevent a mangled bash shell in nix develop
+            # see: https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
+            (pkgs.hiPrio pkgs.bashInteractive)
+          ] ++ commonArgs.nativeBuildInputs;
+          shellHook = ''
+            dot_git="$(git rev-parse --git-common-dir)"
+            if [[ ! -d "$dot_git/hooks" ]]; then
+                mkdir "$dot_git/hooks"
+            fi
+            for hook in misc/git-hooks/* ; do ln -sf "$(pwd)/$hook" "$dot_git/hooks/" ; done
+            ${pkgs.git}/bin/git config commit.template $(pwd)/misc/git-hooks/commit-template.txt
+          '';
+        };
+      };
+    }
+  );
 }
 
---
+++ modules/npcnix.nix
@@ -0,0 +1,43 @@
+{ self }:
+{ config, pkgs, lib, ... }:
+with lib;
+{
+  options.npcnix = {
+    enable = mkOption {
+      default = true;
+      type = types.bool;
+      description = lib.mdDoc ''
+        Enable or disable the npcnix controlling this system.
+      '';
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = self.outputs.packages.${pkgs.system}.npcnix;
+      description = mdDoc "The package providing npcnix binary.";
+    };
+  };
+
+  config = mkIf config.npcnix.enable {
+    environment.systemPackages = [ config.npcnix.package ];
+
+    systemd.services.npcnix = {
+      # restart after successful activation to reload itself, without blocking/terminating whole system activation
+      script = ''
+        ${config.npcnix.package}/bin/npcnix follow --once=activate
+      '';
+
+      wantedBy = [ "multi-user.target" ];
+      after = [ "multi-user.target" ];
+
+      stopIfChanged = false;
+      reloadIfChanged = true;
+      restartIfChanged = false;
+
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = 15;
+      };
+    };
+  };
+}
